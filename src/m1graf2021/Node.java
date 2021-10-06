a9ce1431f48ec22bbd09d4845da8f49136317c66
boubazine <mohammedboubazine28@gmail.com>
2021-10-06 16:12:25 +0200

    implémentation de Edge et Node

--- src/m1graf2021/Node.java
+++ src/m1graf2021/Node.java
@@ -3,15 +3,18 @@ package m1graf2021;
 public class Node implements Comparable<Node> {
     private int id;
     private String name;
+
     public Node(int id){
    this.id = id;
     }
+
     public Node(int id, String name) {
         this.id = id;
         this.name = name;
     }
-public Node(){
-}
+
+    public Node() {
+    }
 
     public int getId() {
         return id;
